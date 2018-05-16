551ee76ece6334220b82b7441254c89208ed1908
HakonHystad <hakonhystad@gmail.com>
2018-05-16 11:08:17 +0200

    finished kinematics class

---
+++ kinematics.h
@@ -0,0 +1,239 @@
+#ifndef _KINEMATICS_H_
+#define _KINEMATICS_H_
+
+#include <string>
+#include <iostream>
+#include <vector>
+
+#include <kdl/chain.hpp>
+#include <kdl/chainfksolver.hpp>
+#include <kdl/chainfksolverpos_recursive.hpp>
+#include <kdl/frames_io.hpp>
+#include <kdl/chainiksolvervel_pinv.hpp>
+#include <kdl/chainiksolverpos_nr_jl.hpp>
+
+
+
+namespace HH
+{
+    using namespace KDL;
+
+    enum Manip{ KR120, KR6 };
+
+
+class Kinematics
+{
+
+public:
+    Kinematics()
+	: n_joints(0)
+	{
+	    // empty
+	}
+    
+    Kinematics( const Manip manipulator, const std::vector<double> &q_home )
+	: Kinematics()
+	{
+	    switch( manipulator )
+	    {
+	    case KR120:
+		setKR120();
+		break;
+	    case KR6:
+		setKR6();
+		break;
+	    }
+
+	    setHome( q_home );
+
+	    for (int i = 0; i < n_joints; ++i)
+		q_guess(i) = this->q_home.at(i);
+
+	}
+
+	
+    ~Kinematics()
+	{
+
+	    if( fksolver != NULL )
+		delete fksolver;
+	    if( iksolver_v != NULL )
+		delete iksolver_v;
+	    if( iksolver != NULL )
+		delete iksolver;
+	}
+
+//////////////////////////////////////////////////////////////
+// return the lastest joint config
+/////////////////////////////////////////////////////////////
+
+    void getJoints( double jointAngles[] )
+	{
+	    for (int i = 0; i < n_joints; ++i)
+	    {
+		jointAngles[i] = q_dest(i);
+	    }
+	}
+
+//////////////////////////////////////////////////////////////
+// perform inverse kinematis 
+/////////////////////////////////////////////////////////////
+// pose is x,y,z in meters and ZYX euler angles A,B,C in radians
+    bool ik( const double pose[6], double jointAngles[] )
+	{
+	    Frame T = Frame( Rotation::EulerZYX( pose[3],pose[4],pose[5]), Vector(pose[0],pose[1],pose[2] ) );
+
+	    if( iksolver->CartToJnt( q_guess, T, q_dest ) != 0 )
+		return false;
+
+	    getJoints( jointAngles );
+
+	    q_guess = q_dest;
+	    
+	    return true;
+
+	}
+//////////////////////////////////////////////////////////////
+// perform forward kinematics on the latest joint config or a given one
+/////////////////////////////////////////////////////////////
+
+    bool fk( double pose[6], const double jointAngles[] = NULL )
+	{
+	    JntArray joints = q_guess;
+
+	    if( jointAngles != NULL )
+	    {
+		for (int i = 0; i < n_joints; ++i)
+		    joints(i) = jointAngles[i];
+	    }
+
+	    Frame T;
+
+	    if( fksolver->JntToCart( joints, T )<0 )	    
+		return false;
+
+	    pose[0] = T.p.x();
+	    pose[1] = T.p.y();
+	    pose[2] = T.p.z();
+
+	    T.M.GetEulerZYX( pose[3], pose[4], pose[5] );
+
+	    
+	    
+	    return true;
+	}
+
+
+
+//////////////////////////////////////////////////////////////
+// specify home joint config
+/////////////////////////////////////////////////////////////
+
+    void setHome( const std::vector<double> &q_home )
+    {
+	this->q_home.clear();
+
+	for (int i = 0; i < n_joints; ++i)
+	{
+	    if( q_home.at(i)<q_lower_lim(i) || q_home.at(i)>q_upper_lim(i) )
+	    {
+		std::cerr << "Home joint angles (radians) exceed the joint limits\n";
+		break;
+	    }
+	    else
+		this->q_home.push_back( q_home.at(i) );
+	}
+    }
+
+
+//////////////////////////////////////////////////////////////
+// use kinamatics for kuka kr120
+/////////////////////////////////////////////////////////////
+
+    void setKR120()
+	{
+	    m_chain = Chain();
+	    
+	    m_chain.addSegment(Segment("link1",Joint( "joint_a1", Vector(0,0,0.675), Vector(0,0,-1), Joint::RotAxis ), Frame( Vector( 0,0,0.675 ) ) ) );
+	    m_chain.addSegment(Segment("link2",Joint( "joint_a2", Vector(0.35,0,0), Vector(0,1,0), Joint::RotAxis ), Frame( Vector( 0.35,0,0 ) ) ) );
+	    m_chain.addSegment(Segment("link3",Joint( "joint_a3", Vector(1.15,0,0), Vector(0,1,0), Joint::RotAxis ), Frame( Vector( 1.15,0,0 ) ) ) );
+	    m_chain.addSegment(Segment("link4",Joint( "joint_a4", Vector(1,0,-0.041), Vector(-1,0,0),  Joint::RotAxis ), Frame( Vector( 1,0,-0.041 ) ) ) );
+	    m_chain.addSegment(Segment("link5",Joint( "joint_a5", Vector(0,0,0), Vector(0,1,0),  Joint::RotAxis ), Frame( Vector( 0,0,0 ) ) ) );
+	    m_chain.addSegment(Segment("link6",Joint( "joint_a6", Vector(0,0,0), Vector(-1,0,0), Joint::RotAxis ), Frame( Vector( 0,0,0 ) ) ) );
+
+	    const double lower[6] = {-3.22885911619, -2.70526034059, -2.26892802759, -6.10865238198, -2.26892802759, -6.10865238198};
+	    const double upper[6] = {3.22885911619, 0.610865238198, 2.68780704807, 6.10865238198, 2.26892802759, 6.10865238198};
+
+	    buildSolvers( lower, upper );
+
+	}
+
+//////////////////////////////////////////////////////////////
+// use kinamatics for kuka kr6
+/////////////////////////////////////////////////////////////
+    
+    void setKR6()
+	{
+	    m_chain = Chain();
+	    
+	    m_chain.addSegment(Segment("link1",Joint( "joint_a1", Vector(0,0,0.4), Vector(0,0,-1), Joint::RotAxis ), Frame( Vector( 0,0,0.4 ) ) ) );
+	    m_chain.addSegment(Segment("link2",Joint( "joint_a2", Vector(0.025,0,0), Vector(0,1,0), Joint::RotAxis ), Frame( Vector( 0.025,0,0 ) ) ) );
+	    m_chain.addSegment(Segment("link3",Joint( "joint_a3", Vector(0.455,0,0), Vector(0,1,0), Joint::RotAxis ), Frame( Vector( 0.455,0,0 ) ) ) );
+	    m_chain.addSegment(Segment("link4",Joint( "joint_a4", Vector(0,0,0.035), Vector(-1,0,0),  Joint::RotAxis ), Frame( Vector( 0,0,0.035 ) ) ) );
+	    m_chain.addSegment(Segment("link5",Joint( "joint_a5", Vector(0.42,0,0), Vector(0,1,0),  Joint::RotAxis ), Frame( Vector( 0.42,0,0 ) ) ) );
+	    m_chain.addSegment(Segment("link6",Joint( "joint_a6", Vector(0.08,0,0), Vector(-1,0,0), Joint::RotAxis ), Frame( Vector( 0.08,0,0 ) ) ) );
+
+	    const double lower[6] = {-2.96705972839, -3.31612557879, -2.09439510239, -3.22885911619, -2.09439510239, -6.10865238198};
+	    const double upper[6] = {2.96705972839, 0.785398163397, 2.72271363311, 3.22885911619, 2.09439510239, 6.10865238198};
+
+	    buildSolvers( lower, upper );
+	}
+
+    
+
+protected:
+
+private:
+    Chain m_chain;
+    JntArray q_dest;
+    JntArray q_guess;
+
+    std::vector<double> q_home;
+
+    int n_joints;
+    JntArray q_lower_lim;
+    JntArray q_upper_lim;
+
+    ChainFkSolverPos_recursive *fksolver = NULL;//Forward position solver
+    ChainIkSolverVel_pinv *iksolver_v = NULL;//Inverse velocity solver
+    ChainIkSolverPos_NR_JL *iksolver = NULL;
+
+    void buildSolvers( const double lower[], const double upper[])
+	{
+	    n_joints = m_chain.getNrOfJoints();
+	    
+	    q_lower_lim = JntArray(n_joints);
+	    q_upper_lim = JntArray(n_joints);
+
+	    q_dest = JntArray(n_joints);
+	    q_guess = JntArray(n_joints);
+	    
+	    for (int i = 0; i < n_joints; ++i)
+	    {
+		q_lower_lim(i) = lower[i];
+		q_upper_lim(i) = upper[i];
+	    }
+
+	    fksolver = new ChainFkSolverPos_recursive( m_chain );
+	    iksolver_v = new ChainIkSolverVel_pinv( m_chain );
+	    iksolver = new ChainIkSolverPos_NR_JL(m_chain, q_lower_lim, q_upper_lim, *fksolver,*iksolver_v,100,1e-6);//Maximum 100 iterations, stop at accuracy 1e-6
+	    
+	}
+
+};
+    
+}// namespace HH
+
+
+
+#endif /* _KINEMATICS_H_ */
--- main.cpp
+++ main.cpp
@@ -1,93 +1,47 @@
-#include <kdl/chain.hpp>
-#include <kdl/chainfksolver.hpp>
-#include <kdl/chainfksolverpos_recursive.hpp>
-#include <kdl/frames_io.hpp>
-#include <kdl/chainiksolvervel_pinv.hpp>
-#include <kdl/chainiksolverpos_nr_jl.hpp>
-#include <stdio.h>
+#include "kinematics.h"
+
+#include <vector>
 #include <iostream>
- 
-using namespace KDL;
- 
  
 int main( int argc, char** argv )
 {
-    //Definition of a kinematic chain & add segments to the chain
-    KDL::Chain chain;
-/*
-    chain.addSegment(Segment(Joint(Joint::RotZ),Frame(Vector(0.0,0.0,1.020))));
-    chain.addSegment(Segment(Joint(Joint::RotX),Frame(Vector(0.0,0.0,0.480))));
-    chain.addSegment(Segment(Joint(Joint::RotX),Frame(Vector(0.0,0.0,0.645))));
-    chain.addSegment(Segment(Joint(Joint::RotZ)));
-    chain.addSegment(Segment(Joint(Joint::RotX),Frame(Vector(0.0,0.0,0.120))));
-    chain.addSegment(Segment(Joint(Joint::RotZ)));
-*/
-    chain.addSegment(Segment("link1",Joint( "joint_a1", Vector(0,0,0.675), Vector(0,0,-1), Joint::RotAxis ), Frame( Vector( 0,0,0.675 ) ) ) );
-    chain.addSegment(Segment("link2",Joint( "joint_a2", Vector(0.35,0,0), Vector(0,1,0), Joint::RotAxis ), Frame( Vector( 0.35,0,0 ) ) ) );
-    chain.addSegment(Segment("link3",Joint( "joint_a3", Vector(1.15,0,0), Vector(0,1,0), Joint::RotAxis ), Frame( Vector( 1.15,0,0 ) ) ) );
-    chain.addSegment(Segment("link4",Joint( "joint_a4", Vector(1,0,-0.041), Vector(-1,0,0),  Joint::RotAxis ), Frame( Vector( 1,0,-0.041 ) ) ) );
-    chain.addSegment(Segment("link5",Joint( "joint_a5", Vector(0,0,0), Vector(0,1,0),  Joint::RotAxis ), Frame( Vector( 0,0,0 ) ) ) );
-    chain.addSegment(Segment("link6",Joint( "joint_a6", Vector(0,0,0), Vector(-1,0,0), Joint::RotAxis ), Frame( Vector( 0,0,0 ) ) ) );
-    // Create solver based on kinematic chain
-//    ChainFkSolverPos_recursive fksolver = ChainFkSolverPos_recursive(chain);
 
-    int n_joints = chain.getNrOfJoints();
-    JntArray q_lower( n_joints );
-    JntArray q_upper( n_joints );
+    std::vector<double> q_home = { 0, -1.57, 1.57, 0, 1.57, 3.1415 };
+    
+    
+    HH::Kinematics kin( HH::KR120, q_home );
 
-    const double lower[] = {-3.22885911619, -2.70526034059, -2.26892802759, -6.10865238198, -2.26892802759, -6.10865238198};
-    const double upper[] = {3.22885911619, 0.610865238198, 2.68780704807, 6.10865238198, 2.26892802759, 6.10865238198};
+    double fk_pose[6];
+    double pose[6] = {1.35, 0, 1.569,  0, 0, -3.1416};
+    double q[6];
 
-    for (int i = 0; i < n_joints; ++i)
-    {
-	q_lower(i) = lower[i];
-	q_upper(i) = upper[i];
-    }
+//////////////////////////////////////////////////////////////
+// Forward kinematics
+/////////////////////////////////////////////////////////////
+
+    double jointAngles[6] = { 0, -1.57, 1.57, 0, 1.57, 3.1415 };
+    if( !kin.fk( fk_pose/*, jointAngles*/ ) )
+	return 1;
+
+    std::cout << "Forward kinematics:\n";
+    for (int i = 0; i < 6; ++i)
+	std::cout << fk_pose[i] << " ";
+    std::cout << std::endl;
+
+//////////////////////////////////////////////////////////////
+// Inverse kinematics
+/////////////////////////////////////////////////////////////
 
     
-    //Creation of the solvers:
-    ChainFkSolverPos_recursive fksolver(chain);//Forward position solver
-    ChainIkSolverVel_pinv iksolver_v(chain);//Inverse velocity solver
-    ChainIkSolverPos_NR_JL iksolver(chain, q_lower, q_upper, fksolver,iksolver_v,100,1e-6);//Maximum 100 iterations, stop at accuracy 1e-6
+    if( !kin.ik( pose, q ) )
+	return 1;
 
-    //Creation of jntarrays:
-    JntArray q_dest(chain.getNrOfJoints());
-    JntArray q_guess(chain.getNrOfJoints());
-
-    const double home_axis[] = {0.0, -90.0, 90.0, 0.0, 90.0, 180.0};
-    for( int i=0; i<6; i++ )
-	q_guess(i) = (home_axis[i] + 5)*(3.141592/180);
-
-    Frame T = Frame( Rotation::EulerZYX(0,0,-3.141592), Vector(1.35,0,1.78) );
-
-    int ret = iksolver.CartToJnt( q_guess, T, q_dest );
-
+    std::cout << "Inverse kinematics:\n";
     for (int i = 0; i < 6; ++i)
     {
-	std::cout << q_dest(i) << " ";
+	std::cout << q[i] << " ";
     }
     std::cout << std::endl;
 
-/*
-    // Assign some values to the joint positions
-    for(unsigned int i=0;i<nj;i++){
-        float myinput;
-        printf ("Enter the position of joint %i: ",i);
-        scanf ("%e",&myinput);
-        jointpositions(i)=(double)myinput;
-    }
- 
-    // Create the frame that will contain the results
-    KDL::Frame cartpos;    
- 
-    // Calculate forward position kinematics
-    bool kinematics_status;
-    kinematics_status = fksolver.JntToCart(jointpositions,cartpos);
-    if(kinematics_status>=0){
-        std::cout << cartpos <<std::endl;
-        printf("%s \n","Succes, thanks KDL!");
-    }else{
-        printf("%s \n","Error: could not calculate forward kinematics :(");
-    }
-    */
+    return 0;
 }
