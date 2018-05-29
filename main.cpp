7003941260349c5c0f2482ab7f6ede2be0015fc6
HakonHystad <hakonhystad@gmail.com>
2018-05-29 21:50:02 +0200

    added trajectory planning

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@
 #include <vector>
 #include <unistd.h>
 
+#define N_STATES 12
+
+void ramp( const std::vector<double> &pos1, const std::vector<double> &pos2, std::vector<double> &out, double t, double tf );
+
 int main( int argc, char** argv )
 {
 
@@ -21,24 +25,50 @@ int main( int argc, char** argv )
     std::vector<double> base = {0,0,0,-3.141592/2,0,0};
     std::vector<double> tool = {0,0,0,0,0,0};
 
-    std::vector<double> start = {1.35092, 0, 1.784, 0.01, 0, 0, 3.141592, 3.141592/2, 0, 0.0873, -0.0873, 0};
+    std::vector<double> start = {1.35092, 0, 1.784, 0, 0, 0, 3.141592, 3.141592/2, 0, 0, 0, 0};
+    auto end = start;
+    end.at(1) += 0.5;
+    
     auto home_axis = HH::home_axis_kr120;
     home_axis.at(0) -= 3.141592/2;
+    
     HH::RSI rsi( start, home_axis, base, tool, HH::Manip::KR120, "4950" );
     
     rsi.start();
 
-    sleep(60);
+    auto pose = start;
 
-    double tmp = start.at(0);
-    start.at(0) = start.at(1);
-    start.at(1) = tmp;
+    double timeOfPath = 5;// s
+
+    // send in intervals of 0.1s
+    for (int i = 0; i < timeOfPath/0.1; ++i)
+    {
+	ramp( start, end, pose, i*0.1, timeOfPath );
+	rsi.setPose( pose );
+	sleep(0.1);
+    }
     
-    rsi.setPose( start );
-
-    sleep(2);
 
     rsi.end();
     
     return 0;
+}
+
+double cubic( a0, qf, t, tf )
+{
+    // a1 = 0
+    double a2 = -1.5*( qf - a0 )/( (tf-1.5)*tf );
+    double a3 = ( qf - a0 )/( (tf-1.5)*tf*tf );
+
+    return (a3*t*t*t + a2*t*t + a0);
+}
+
+
+void ramp( const std::vector<double> &pos1, const std::vector<double> &pos2, std::vector<double> &out, double t, double tf )
+{
+     for (int i = 0; i < N_STATES; ++i)
+     {
+	 out[i] = cubic( pos1[i], pos2[i], t, tf );
+     }
+
 }
